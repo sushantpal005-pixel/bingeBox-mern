276f9791ecfb5b4fcda5785838bcff42ca185249
sushantpal005-pixel <sushantpal005@gmail.com>
2026-03-29 22:53:43 +0530

    production ready app

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,14 @@ import databaseConnection from "./utils/database.js"
 import cookieParser from "cookie-parser"
 import userRoute from "./routes/userRoute.js"
 import cors from "cors"
+import path from "path"
 
 dotenv.config({
     path: "./.env"
 })
 databaseConnection()
 const app = express()
+const _dirname = path.resolve()
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
@@ -22,6 +24,13 @@ app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 //api
 app.use("/api/v1/user", userRoute)
 
+app.use(express.static(path.join(_dirname, "/frontend/dist")))
+// app.get('/*', (_, res)=>{
+//     res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"))
+// })
+app.use((_, res) => {
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+})
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is listen at port ${process.env.PORT} `)
