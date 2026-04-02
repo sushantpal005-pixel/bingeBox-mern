5709cb0cefd09f3f3d378072e8233f905f1e49fe
sushantpal005-pixel <sushantpal005@gmail.com>
2026-04-02 23:52:47 +0530

    add responsiveness in mainContainer.jsx

--- frontend/src/components/MainContainer.jsx
+++ frontend/src/components/MainContainer.jsx
@@ -1,3 +1,35 @@
+// import React from 'react'
+// import VideoTitle from './VideoTitle'
+// import VideoBackground from './VideoBackground'
+// import { useSelector } from 'react-redux'
+
+
+// const MainContainer = () => {
+//   const movie = useSelector(store => store.movie?.nowPlayingMovies)
+//   const movieId = useSelector(store => store.movie.selectedMovie)
+  
+//   if (!movie) return;   //early return in react
+//   // if (!movieId) {
+//   //   console.log("bhai",movie)
+//   //   const { overview, id, title } = movie[4]
+//   // }
+//   // const {overview, id, title} = movie[movieId]
+//   const currentMovie =
+//     movie.find((m) => m.id === movieId) || movie[4];
+
+//   const { overview, id, title } = currentMovie;
+
+//   return (
+//     <div >
+//       <VideoTitle title={title} overview={overview} /> 
+//        <VideoBackground movieId={id} /> 
+//     </div>
+//   )
+// }
+
+// export default MainContainer
+
+
 import React from 'react'
 import VideoTitle from './VideoTitle'
 import VideoBackground from './VideoBackground'
@@ -20,7 +52,7 @@ const MainContainer = () => {
   const { overview, id, title } = currentMovie;
 
   return (
-    <div >
+    <div className='relative w-full h-[70vh] sm:h-[80vh] md:h-[90vh] lg:h-screen overflow-hidden' >
       <VideoTitle title={title} overview={overview} /> 
        <VideoBackground movieId={id} /> 
     </div>
