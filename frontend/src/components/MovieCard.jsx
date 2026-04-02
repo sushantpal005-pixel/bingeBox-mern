93b5c611e1e0218e76b7f716e447ca7b70441ebc
sushantpal005-pixel <sushantpal005@gmail.com>
2026-04-02 23:53:06 +0530

    add responsiveness

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,21 +1,54 @@
-import React from 'react'
-import { TMDB_IMG_URL } from '../utils/constant'
-import { useDispatch } from 'react-redux'
-import { getId, setOpen, setSelectedMovie } from '../redux/movieSlice'
+// import React from 'react'
+// import { TMDB_IMG_URL } from '../utils/constant'
+// import { useDispatch } from 'react-redux'
+// import { getId, setOpen, setSelectedMovie } from '../redux/movieSlice'
 
-const MovieCard = ({posterPath, movieId}) => {
-  const dispatch = useDispatch()
-  if(posterPath === null) return null
-  const handleOpen = ()=>{
-    dispatch(getId(movieId))
-    dispatch(setOpen(true))
-    dispatch(setSelectedMovie(movieId))
-  }
+// const MovieCard = ({posterPath, movieId}) => {
+//   const dispatch = useDispatch()
+//   if(posterPath === null) return null
+//   const handleOpen = ()=>{
+//     dispatch(getId(movieId))
+//     dispatch(setOpen(true))
+//     dispatch(setSelectedMovie(movieId))
+//   }
+//   return (
+//     <div className='w-48 pr-2' onClick={handleOpen} >
+//       <img src={`${TMDB_IMG_URL}/${posterPath}`} alt="movie-banner" />
+//     </div>
+//   )
+// }
+
+// export default MovieCard
+
+
+import React from "react";
+import { TMDB_IMG_URL } from "../utils/constant";
+import { useDispatch } from "react-redux";
+import { getId, setOpen, setSelectedMovie } from "../redux/movieSlice";
+
+const MovieCard = ({ posterPath, movieId }) => {
+  const dispatch = useDispatch();
+
+  if (!posterPath) return null;
+
+  const handleOpen = () => {
+    dispatch(getId(movieId));
+    dispatch(setOpen(true));
+    dispatch(setSelectedMovie(movieId));
+  };
+
   return (
-    <div className='w-48 pr-2' onClick={handleOpen} >
-      <img src={`${TMDB_IMG_URL}/${posterPath}`} alt="movie-banner" />
+    <div
+      onClick={handleOpen}
+      className="w-32 sm:w-36 md:w-44 lg:w-48 flex-shrink-0 cursor-pointer transform hover:scale-110 transition duration-300"
+    >
+      <img
+        src={`${TMDB_IMG_URL}/${posterPath}`}
+        alt="movie-banner"
+        className="w-full h-full object-cover rounded-md shadow-md"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
