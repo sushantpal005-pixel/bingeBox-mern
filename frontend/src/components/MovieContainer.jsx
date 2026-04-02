5c72e50d706e171f6818efd29de5b20c722dd1e4
sushantpal005-pixel <sushantpal005@gmail.com>
2026-04-02 23:53:12 +0530

    add responsiveness

--- frontend/src/components/MovieContainer.jsx
+++ frontend/src/components/MovieContainer.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
-import MovieList from './MovieList'
-import { useSelector } from 'react-redux'
+// import React from 'react'
+// import MovieList from './MovieList'
+// import { useSelector } from 'react-redux'
+
+// const MovieContainer = () => {
+//   const movie = useSelector(store=>store.movie)
+//   //console.log(movie)
+//   return (
+//     <div className='bg-black'>
+//       <div className='-mt-52 relative z-10'>
+//         <MovieList title = {"Popular Movies"} movies={movie.popularMovies} />
+//         <MovieList title = {"Now Playing Movies"} movies={movie.nowPlayingMovies} />
+//         <MovieList title = {"Top Rated Movies"} movies={movie.topRatedMovies} />
+//         <MovieList title = {"Upcoming Movies"} movies={movie.upcomingMovies} /> 
+//       </div>
+//     </div>
+//   )
+// }
+
+
+// export default MovieContainer
+
+
+import React from "react";
+import MovieList from "./MovieList";
+import { useSelector } from "react-redux";
 
 const MovieContainer = () => {
-  const movie = useSelector(store=>store.movie)
-  //console.log(movie)
+  const movie = useSelector((store) => store.movie);
+
   return (
-    <div className='bg-black'>
-      <div className='-mt-52 relative z-10'>
-        <MovieList title = {"Popular Movies"} movies={movie.popularMovies} />
-        <MovieList title = {"Now Playing Movies"} movies={movie.nowPlayingMovies} />
-        <MovieList title = {"Top Rated Movies"} movies={movie.topRatedMovies} />
-        <MovieList title = {"Upcoming Movies"} movies={movie.upcomingMovies} /> 
-      </div>
+    <div className="bg-black relative z-10 -mt-20 sm:-mt-28 md:-mt-36"> 
+      <MovieList title={"Popular Movies"} movies={movie.popularMovies} />
+      <MovieList title={"Now Playing Movies"} movies={movie.nowPlayingMovies} />
+      <MovieList title={"Top Rated Movies"} movies={movie.topRatedMovies} />
+      <MovieList title={"Upcoming Movies"} movies={movie.upcomingMovies} />
     </div>
-  )
-}
+  );
+};
 
-
-export default MovieContainer
+export default MovieContainer;
