60b56733b6f37d5879e0fe807b3b9b6713f9cc26
sushantpal005-pixel <sushantpal005@gmail.com>
2026-04-03 13:22:10 +0530

    add responsiveness

--- frontend/src/components/MovieContainer.jsx
+++ frontend/src/components/MovieContainer.jsx
@@ -29,7 +29,7 @@ const MovieContainer = () => {
   const movie = useSelector((store) => store.movie);
 
   return (
-    <div className="bg-black relative z-10 -mt-20 sm:-mt-28 md:-mt-36"> 
+    <div className="bg-black relative z-10 -mt-2 sm:-mt-28 md:-mt-36"> 
       <MovieList title={"Popular Movies"} movies={movie.popularMovies} />
       <MovieList title={"Now Playing Movies"} movies={movie.nowPlayingMovies} />
       <MovieList title={"Top Rated Movies"} movies={movie.topRatedMovies} />
